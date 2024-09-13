076a01bd68f89f2f96f4a6280760f6c8497b283e
anastasiat <anastasia.tobohova@yandex.ru>
2024-09-13 18:01:55 +0300

    09/09

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
-    if my_list [index] < 0:
+    number = my_list[index]
+    if number < 0:
         break
-    print(my_list[index])
+    if number > 0:
+        print(number)
     index += 1
 
 
