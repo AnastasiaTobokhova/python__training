34e4691836bf35ecb1c671543de6e9fca838cc83
anastasiat <anastasia.tobohova@yandex.ru>
2024-11-04 15:39:34 +0300

    04/11

--- module_8_1.py
+++ module_8_1.py
@@ -1,11 +1,11 @@
-def add_everything_up(a,b):
-    return a + b
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except TypeError:
+        return str(a) + str(b)
+    else:
+        return result
 
-try:
-    print(add_everything_up(123.456, 'строка'))
-    print(add_everything_up('яблоко', 4215))
-    print(add_everything_up(123.456, 7))
-except TypeError as exp:
-    print(f'Что-то пошло не так {exp}')
-except SyntaxError as exp_2:
-    print(f'Что-то пошло не так {exp_2}')
+print(add_everything_up(123.456, 'строка'))  # Вывод: 123.456строка
+print(add_everything_up('яблоко', 4215))     # Вывод: яблоко4215
+print(add_everything_up(123.456, 7))         # Вывод: 130.456
